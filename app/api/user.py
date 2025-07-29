2d2d3e243bd2240b385015b7da3f8d46b61f9fa5
AviadGilboa <Gilboa.Aviad@gmail.com>
2025-07-29 12:51:08 +0300

    Add premmisions for user get method

--- app/api/user.py
+++ app/api/user.py
@@ -3,7 +3,7 @@ import fastapi
 from sqlalchemy.orm import Session
 
 from .. import schemas
-from ..utils import hash
+from ..utils import hash, oauth2
 from ..db import models
 from ..db.session import get_db
 
@@ -45,14 +45,19 @@ def create_user(
 def get_user_by_id(
     id: int,
     db: Session=fastapi.Depends(get_db),
+    current_user: schemas.TokenData = fastapi.Depends(oauth2.get_current_user)
 ):
-    user = db.get(
-        entity=models.User,
-        ident=id,
-    )
-    if not user:
-        raise fastapi.HTTPException(
-            status_code=fastapi.status.HTTP_404_NOT_FOUND,
-            detail=f'User with id: {id} does not exist',
+    if current_user.role is 'admin' or current_user.id == id:
+        user = db.get(
+            entity=models.User,
+            ident=id,
         )
-    return user
+        if not user:
+            raise fastapi.HTTPException(
+                status_code=fastapi.status.HTTP_404_NOT_FOUND,
+                detail=f'User with id: {id} does not exist',
+            )
+        return user
+    raise fastapi.HTTPException(
+        fastapi.status.HTTP_401_UNAUTHORIZED,
+    )
